2a14bd7e9055d0d84fe03d7dfddb74cbf8c342d3
lebrancconvas <lebrancconvas@gmail.com>
2022-01-19 00:03:30 +0700

    💄 Creating Face on UI.

--- src/App.java
+++ src/App.java
@@ -23,13 +23,24 @@ class Window extends JPanel {
     }
 
     public void paintComponent(Graphics graphic) {
+        // Background Color.
         graphic.setColor(Color.BLUE);
         graphic.fillRect(0, 0, 800, 600);
 
+        // Text Message.  
         graphic.setColor(Color.WHITE);
-        graphic.drawString("Your Program is running.", 80, 100);
+        graphic.drawString("This Program is running successfully.", 80, 100);
         graphic.drawString("This is the graphical program for anyone who want to approve their skill.", 80, 120);
         graphic.drawString("JAVA SWING PLAYGROUND!!", 80, 180);
+        graphic.drawString("Hope you all enjoy with the program.", 80, 250);
+
+        // Face Sketching. 
+        graphic.setColor(Color.PINK);
+        graphic.drawOval(350, 400, 100, 100);
+        graphic.drawOval(370, 420, 20, 20);
+        graphic.drawOval(410, 420, 20, 20);
+        graphic.drawLine(390, 460, 410, 480);
+        graphic.drawLine(390, 490, 410, 450);
     }
 }
 

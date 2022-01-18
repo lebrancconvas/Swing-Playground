58bc4b6909cd5d0d749d555531d7c58dd09e11bd
lebrancconvas <lebrancconvas@gmail.com>
2022-01-18 20:36:34 +0700

    ✨ Create Java Swing Window.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import javax.swing.*;
+import java.awt.*;
+
+public class App extends JPanel {
+    public static void main(String[] args) {
+        App m = new App();
+        JFrame f = new JFrame();
+        f.add(m);
+        f.setTitle("Java Swing Playground.");
+        f.setSize(800, 600);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
     }
 }
+
+

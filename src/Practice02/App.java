b9b54ae5e18ab3ce43a17b00cc1cd9f1ded43615
lebrancconvas <lebrancconvas@gmail.com>
2022-01-20 04:39:03 +0700

    ✨ Create Line with Plot via Digital Differential Analyzer (DDA) Algorithm.

---
+++ src/Practice02/App.java
@@ -0,0 +1,70 @@
+package Practice02;
+
+import javax.swing.*;
+import java.awt.*;
+// import java.lang.*;
+
+public class App {
+	public static void main(String[] args) {
+		Window SwingWindow = new Window();
+		SwingWindow.start();
+	}
+}
+
+class Window extends JPanel {
+	public void start() {
+		JFrame frame = new JFrame();
+
+		int WIDTH = 800;
+		int HEIGHT = 600;
+
+		frame.add(new Window());
+		frame.setSize(WIDTH, HEIGHT);
+		frame.setTitle("02: Drawing Line.");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+
+	public void paintComponent(Graphics graphic) {
+		graphic.setColor(Color.BLACK);
+		graphic.fillRect(0, 0, WIDTH, HEIGHT);
+		graphic.setColor(Color.WHITE);
+		ddaLine(graphic, 100, 50, 10, 70);
+	}
+
+	public void ddaLine(Graphics graphic, int x1, int y1, int x2, int y2) {
+		float deltaX = x2 - x1;
+		float deltaY = y2 - y1;
+		float slope = deltaY / deltaX;
+		
+
+		if(slope >= 0 && slope <= 1) {
+			for(int i = x1; i <= x2; i++) {
+				plot(graphic, i, y1);
+				y1 += (int) Math.ceil(slope);
+			}
+		}
+		else if(slope > 1) {
+			for(int i = y1; i <= y2; i++) {
+				plot(graphic, x1, i);
+				y1 += (int) Math.ceil(1 / slope); 
+			}
+		}
+		else if(slope >= -1 && slope <= 0) {
+			for(int i = x1; i >= x2; i--) {
+				plot(graphic, i, y1);
+				y1 += (int) Math.floor(slope); 
+			}
+		}
+		else if(slope < -1) {
+			for(int i = y1; i >= y2; i--) {
+				plot(graphic, x1, i);
+				y1 += (int) Math.floor(1 / slope);  
+			}
+		}
+	}
+
+	public void plot(Graphics graphic, int x, int y) {
+		graphic.fillRect(x, y, 1, 1);
+	}
+}
